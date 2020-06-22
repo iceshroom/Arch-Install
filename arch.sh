6e0b82570180761b4d549fc9366600b56ab6ac87
iceshroom <55022746+iceshroom@users.noreply.github.com>
2020-06-23 00:24:53 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -214,6 +214,10 @@ diskpart()
     local FDISK_T=''
     local FDISK_REP=''
 
+    if [ "${#PARTITION_TO_MAKE[@]}" == 0 ] ; then
+    	echo -e "${red}No partition was specified! Use -p option!${plain}"
+	exit 1
+    fi
 
     echo -e "${green}Here we go!${plain}"
 
